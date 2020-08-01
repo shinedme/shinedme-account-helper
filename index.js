9f7c90f0e25596c341ecf4427e32930695d17c37
Bo Yao <bo@nearprotocol.com>
2020-07-31 19:58:10 -0700

    fix

--- index.js
+++ index.js
@@ -24,26 +24,31 @@ cryptoWaitReady().then(async () => {
     const alice = keyring.addFromUri('//Alice', { name: 'Alice default' });
 
     app.post('/', async (req, res) => {
-        let addr = req.body.addr;
         // The actual address that we will use
-        const ADDR = '5DTestUPts3kjeXSTMyerHihn1uwMfLj8vU8sqF7qYrFabHE';
+        try {
+            let addr = req.body.addr;
+            console.log(addr)
+            console.log(req.body)
+            // Retrieve the last timestamp
+            const now = await api.query.timestamp.now();
 
-        // Retrieve the last timestamp
-        const now = await api.query.timestamp.now();
+            // Retrieve the account balance & nonce via the system module
+            const { nonce, data: balance } = await api.query.system.account(addr);
 
-        // Retrieve the account balance & nonce via the system module
-        const { nonce, data: balance } = await api.query.system.account(ADDR);
+            console.log(`${now}: balance of ${balance.free} and a nonce of ${nonce}`);
+            const txHash = await
+                api.tx.balances
+                    .transfer(addr, 100000000000)
+                    .signAndSend(alice);
 
-        console.log(`${now}: balance of ${balance.free} and a nonce of ${nonce}`);
-        const txHash = await
-            api.tx.balances
-                .transfer(addr, 1000000)
-                .signAndSend(alice);
-
-        // Show the hash
-        // const txHash = await api.rpc.system.chain();
-        console.log(`Submitted with hash ${txHash}`);
-        res.json({ txHash })
+            // Show the hash
+            // const txHash = await api.rpc.system.chain();
+            console.log(`Submitted with hash ${txHash}`);
+            // console.log(await api.query.system.account(addr));
+            res.json({ txHash })
+        } catch (e) {
+            res.status(400).json({ 'error': e.toString() })
+        }
     })
     app.get('/', (req, res) => res.send('Hello World!'))
     app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
