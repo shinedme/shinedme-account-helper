7ed7345d973d3af6aaee58adce4f88950357bf8b
Bo Yao <bo@nearprotocol.com>
2020-08-07 21:42:58 -0700

    cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
+
 const port = 4000
 
 const { cryptoWaitReady } = require('@polkadot/util-crypto');
